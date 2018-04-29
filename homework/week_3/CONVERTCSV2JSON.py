51afc4038faa040d2509018125a2b290a6c70550
noamrubin22 <36198454+noamrubin22@users.noreply.github.com>
2018-04-29 17:43:52 +0200

    Update CONVERTCSV2JSON.py

--- homework/week_3/CONVERTCSV2JSON.py
+++ homework/week_3/CONVERTCSV2JSON.py
@@ -1,22 +1,18 @@
+# Name: Noam Rubin
+# Course: Dataprocessing
+# Date: 29-04-2018
+#
+# This script converts data from csv to JSON
+
 import csv 
 import json
 
 # read csv file
 with open("huishoudens.csv", "r") as csv_file:
-	reader = csv.DictReader(csv_file)
-	clean_reader = reader
-	rows = list(csv_file)
-
-	for elem in rows:
-		clean_rows =rows.extend(elem.strip().split(";"))
-
-	print(clean_rows)
-
-
-
-
-# # write json file
-# with open("huishoudens.json", "w") as json_file:
-# 	json.write(" { data :")
-# 	json.dump(rows, json_file, indent=4)
-# 	json.write("}")
+	fields = ["year", "totaal", "zonder"]
+	reader = csv.DictReader(csv_file, fields)
+	data = {[row for row in reader]}
+	
+# write json file
+with open("huishoudens.json", "w") as json_file:
+ 	json.dump(data,json_file)
